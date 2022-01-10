5375e1860c1095518911de74ac891388cfa13a2a
Francis Guido <fjguido97@gmail.com>
2022-01-10 17:16:58 -0500

    commit tree 0be9a580a0971f87c4ff5fe817cc356ae194b4ca

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+  }
+function distanceFromHqInFeet(someValue) {
+  
+  return distanceFromHqInBlocks(someValue) * 264;
+    
+}
+
+function distanceTravelledInFeet(start, destination) {
+  let r = Math.abs(destination - start) * 264;
+  return r 
+
+}
+
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+  let feetDistance = distanceTravelledInFeet(start, destination);
+
+  if (feetDistance <= 400){
+    return 0;
+  }
+   else if (feetDistance >= 2500) {
+      return 'cannot travel that far';
+  }
+  else if (feetDistance >= 2000) {
+    return 25;
+  }
+
+  else if (feetDistance <= 2000) {
+    return (feetDistance-400) * 0.02;
+  }
+}
